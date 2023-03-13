060861e4cacec701a278a16b26ae92e61b99f242
Khushboo Agarwal <khushbooagarwal.cse24@jecrc.ac.in>
2023-03-13 21:19:51 +0530

    database

--- config/db.config.js
+++ config/db.config.js
@@ -1,17 +1,17 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 function connect() {
-   try {
-     mongoose.connect(
-       "mongodb+srv://khushboo123:SIH123@phantom.agz12to.mongodb.net/authentication"
-     );
+  try {
+    mongoose.connect(
+      "mongodb+srv://Phantom123:Phantom123@bootcamp.dzafsmy.mongodb.net/?retryWrites=true&w=majority"
+    );
 
-     var db = mongoose.connection;
-     db.on("connected", console.error.bind(console, "MongoDB connection done"));
-     db.on("error", console.error.bind(console, "MongoDB connection error:"));
-   } catch (error) {
-     console.log(error);
-   }
+    var db = mongoose.connection;
+    db.on("connected", console.error.bind(console, "MongoDB connection done"));
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = connect;
